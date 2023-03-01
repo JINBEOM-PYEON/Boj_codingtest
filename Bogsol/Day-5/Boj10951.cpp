feb174da173d8229123b65bbd5b2a27af2c3d39a
편진범 <hapy1122@naver.com>
2023-03-01 05:55:26 +0000

    Boj No.10951

--- Bogsol/Day-5/Boj10951.cpp
+++ Bogsol/Day-5/Boj10951.cpp
@@ -11,4 +11,15 @@ int main(int argc, const char *argv[]) {
 	return 0;
 }
 
-/* */
+/*#include <iostream>
+using namespace std;
+ 
+int main(int argc, const char *argv[]) {
+ 
+	int a, b;
+	while (!(cin >> a >> b).eof()) {	// 혹은 eof() 대신 fail()을 사용해도 된다.
+		cout << a + b << "\n";
+	}
+ 
+	return 0;
+} */
