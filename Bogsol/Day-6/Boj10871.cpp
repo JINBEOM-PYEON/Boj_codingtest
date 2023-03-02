51439cd85085e81d6f1ad9c148a2e3a32b30e2ce
편진범 <hapy1122@naver.com>
2023-03-02 03:26:31 +0000

    Boj No.10871

--- Bogsol/Day-6/Boj10871.cpp
+++ Bogsol/Day-6/Boj10871.cpp
@@ -14,4 +14,19 @@ int main() {
 	}
 	return 0;
 }
-/* 
+/* int main() {
+ 
+	int N;
+	int X;
+	cin >> N >> X;	// N과 X 입력
+ 
+	int val;
+	for(int i = 0; i < N; i++) {
+		// 입력을 받은 값이 X보다 작을 경우 출력
+		cin >> val;
+		if(val < X) {
+			cout << val;
+		}
+	}
+ 
+}
